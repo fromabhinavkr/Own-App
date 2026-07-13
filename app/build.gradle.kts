8eafb9c8305f70167a2b294083adb67d8f5531f2
Abhinav K R <abhinavnowkr@gmail.com>
2026-07-13 19:01:52 +0530

    Added Private Browser

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 4
-        versionName = "4.6.0"
+        versionName = "4.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
